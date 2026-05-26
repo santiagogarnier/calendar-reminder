c43e7e36782e98fc8c86e9812a7878e75dce294f
Santi Garnier <108945065+santiagogarnier@users.noreply.github.com>
2026-05-26 09:14:27 -0300

    feat: funciona la lectura de eventos del calendario

--- popup.js
+++ popup.js
@@ -7,13 +7,31 @@ btnLogin.addEventListener("click",()=>{
 })
 
 function login(){
-    chrome.identity.getAuthToken({"interactive": true},(token)=>{
+    chrome.identity.getAuthToken({"interactive": true},async (token)=>{
         if(chrome.runtime.lastError){
             status.textContent = "Error al conectar: " + chrome.runtime.lastError.message;
             return;
         }
 
         status.textContent = "Conectado";
-        console.log("Token obtenido", token);
+        const eventos = await obtenerEventos(token);
+        console.log("Eventos:", eventos);
     })
+}
+
+async function obtenerEventos(token){
+    const hoy = new Date();
+    const enCatDias = new Date();
+    enCatDias.setDate(hoy.getDate() + 14);
+
+    const url = `https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${hoy.toISOString()}&timeMax=${enCatDias.toISOString()}&singleEvents=true&orderBy=startTime`;
+
+    const respuesta = await fetch(url, {
+        headers: {
+            Authorization : `Bearer ${token}`
+        }
+    });
+
+    const datos = await respuesta.json();
+    return datos.items;
 }
