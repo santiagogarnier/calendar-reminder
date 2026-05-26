77785b3de9ab67e66dda01df44637b3cac7277a0
Santi Garnier <108945065+santiagogarnier@users.noreply.github.com>
2026-05-25 23:56:08 -0300

    feat: se agregan estructuras de popup

---
+++ popup.js
@@ -0,0 +1,19 @@
+const btnlogin = document.getElementById("btn-login");
+const status = document.getElementById("status");
+
+//Usuario clickea el boton 
+btnLogin.addEventListener("click",()=>{
+    login();
+})
+
+function login(){
+    chrome.identity.getAuthToken({"interactive": true},(token)=>{
+        if(chrome.runtime.lastError){
+            status.textContent = "Error al conectar: " + chrome.runtime.lastError.message;
+            return;
+        }
+
+        status.textContent = "Conectado";
+        console.log("Token obtenido", token);
+    })
+}
